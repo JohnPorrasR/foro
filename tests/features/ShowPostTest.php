5619a9d90c9872fc2b3debca95cecb481fe41366
JohnPorrasR <johnporrasr@gmail.com>
2017-07-14 23:26:06 -0500

    mostrar el detalle de un registro con TDD

---
+++ tests/features/ShowPostTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ShowPostTest extends TestCase
+{
+
+    function test_a_user_can_see_the_post_details()
+    {
+        // having
+        $user = $this->defaultUser([
+            'name' => 'John Porras',
+        ]);
+
+        $post = factory(\App\Post::class)->make([
+                'title' => 'Este es el titulo del post',
+                'content' => 'Este es el contenido del post'
+        ]);
+
+        $user->posts()->save($post);
+
+        // when
+        $this->visit(route('posts.show', $post))
+                ->seeInElement('h1', $post->title)
+                ->see($post->content)
+                ->see($user->name);
+    }
+}
