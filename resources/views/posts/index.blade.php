41f45c7cdd75e50766d5c089f0bb38bcf916913e
JohnPorrasR <johnporrasr@gmail.com>
2017-08-03 21:22:49 -0500

    listando los posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Posts</h1>
+
+    <ul>
+        @foreach($posts as $p)
+            <li>
+                <a href="{{ $p->url }}">{{ $p->title }}</a>
+            </li>
+        @endforeach
+    </ul>
+
+@endsection
